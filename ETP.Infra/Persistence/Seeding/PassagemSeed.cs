38a93c9a9bdd91ed9eb7c1d4929384711c0369d1
Andre <pereira.al.andre@gmail.com>
2023-11-13 17:58:58 -0300

    Ajustes

--- ETP.Infra/Persistence/Seeding/PassagemSeed.cs
+++ ETP.Infra/Persistence/Seeding/PassagemSeed.cs
@@ -24,12 +24,16 @@ namespace ETP.Infra.Persistence.Seeding
             {
                 PassagemJsonSchema seed = new PassagemJsonSchema(new List<PassagemJson>()
                 {
-                    new PassagemJson("EVO01", "HPZ-0292", "Renault", "Sandero", new DateTime(2023, 10, 13, 08, 00, 00), new DateTime(2023, 10, 13, 09, 00, 00), "DIN"),
-                    new PassagemJson("EVO01", "HPZ-0292", "Renault", "Sandero", new DateTime(2023, 10, 13, 08, 00, 00), new DateTime(2023, 10, 13, 10, 00, 00), "MEN"),
-                    new PassagemJson("EVO01", "HPZ-0292", "Renault", "Sandero", new DateTime(2023, 10, 13, 08, 00, 00), new DateTime(2023, 10, 13, 11, 50, 00), "PIX"),
-                    new PassagemJson("EVO01", "HPZ-0292", "Renault", "Sandero", new DateTime(2023, 10, 13, 08, 00, 00), new DateTime(2023, 10, 13, 09, 30, 00), "CDE"),
-                    new PassagemJson("EVO01", "HPZ-0292", "Renault", "Sandero", new DateTime(2023, 10, 13, 08, 00, 00), new DateTime(2023, 10, 14, 09, 00, 00), "CCR"),
-                    new PassagemJson("EVO01", "HPZ-0292", "Renault", "Sandero", new DateTime(2023, 10, 13, 08, 00, 00), new DateTime(2023, 10, 13, 17, 00, 00), "TAG"),
+                    new PassagemJson("EVO01", "AAA-0292", "Renault", "Sandero", new DateTime(2023, 10, 13, 08, 00, 00), new DateTime(2023, 10, 13, 09, 00, 00), "DIN"),
+                    new PassagemJson("EVO01", "BBB-0292", "Renault", "Sandero", new DateTime(2023, 10, 13, 08, 00, 00), new DateTime(2023, 10, 13, 10, 00, 00), "MEN"),
+                    new PassagemJson("EVO01", "AAA-0292", "Renault", "Sandero", new DateTime(2023, 10, 13, 08, 00, 00), new DateTime(2023, 10, 13, 11, 50, 00), "PIX"),
+                    new PassagemJson("PLJK01", "BBB-0292", "Renault", "Sandero", new DateTime(2023, 10, 13, 08, 00, 00), new DateTime(2023, 10, 13, 09, 30, 00), "CDE"),
+                    new PassagemJson("SZJK01", "CCC-0292", "Renault", "Sandero", new DateTime(2023, 10, 13, 08, 00, 00), new DateTime(2023, 10, 14, 09, 00, 00), "CCR"),
+                    new PassagemJson("PLJK01", "FFF-0292", "Renault", "Sandero", new DateTime(2023, 10, 13, 08, 00, 00), new DateTime(2023, 10, 13, 17, 00, 00), "TAG"),
+                    new PassagemJson("SZJK01", "EEE-0292", "Renault", "Sandero", new DateTime(2023, 10, 13, 08, 00, 00), new DateTime(2023, 10, 13, 09, 30, 00), "CDE"),
+                    new PassagemJson("SZJK01", "GGG-0292", "Renault", "Sandero", new DateTime(2023, 10, 13, 08, 00, 00), new DateTime(2023, 10, 13, 10, 00, 00), "MEN"),
+                    new PassagemJson("PLJK01", "EEE-0292", "Renault", "Sandero", new DateTime(2023, 10, 13, 08, 00, 00), new DateTime(2023, 10, 14, 09, 00, 00), "CCR"),
+                    new PassagemJson("SZJK01", "AAA-0292", "Renault", "Sandero", new DateTime(2023, 10, 13, 08, 00, 00), new DateTime(2023, 10, 13, 17, 00, 00), "TAG"),
                 });
                 List<Passagem> entity = new();
 
